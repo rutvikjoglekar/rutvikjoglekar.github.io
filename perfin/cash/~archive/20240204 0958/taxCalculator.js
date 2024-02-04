47a8123a7f0f5d4ffbba3f6ac97e7bb7ed6bf457
rutvikjoglekar <joglekar.rutvik@gmail.com>
2024-02-04 14:12:46 -0600

    20240204 1412 commit

---
+++ perfin/cash/~archive/20240204 0958/taxCalculator.js
@@ -0,0 +1,93 @@
+
+
+// JavaScript function for tax calculation
+    function calculateTax() {
+        const income1 = parseFloat(document.getElementById('income1').value);
+        const income2Input = document.getElementById('income2').value;
+        const income2 = income2Input ? parseFloat(income2Input) : 0;
+        const state = document.getElementById('state').value;
+        const filingstatus = document.getElementById('filingstatus').value;
+            
+        const income = income1 + income2;
+        
+        // Fed income with deduction
+        if (filingstatus === 'mfj') {
+            fedInc = income - 29000;
+        } else if (filingstatus === 'single') {
+            fedInc = income - 14600;
+        }
+
+        // State income with deduction
+        const stateInc = income
+
+        if (isNaN(income) || state === 'none') {
+            document.getElementById('result').innerHTML = 'Please enter valid income and select a state.';
+            return;
+        }
+
+        // Federal tax rate based on income brackets
+        let federalTax;
+        if (filingstatus === 'single') {
+            if (fedInc <= 11600) {
+                federalTax = (fedInc * 0.10);
+            } else if (fedInc <= 47150) {
+                federalTax = (1160 + (fedInc - 11600) * 0.12);
+            } else if (fedInc <= 100525) {
+                federalTax = (5426 + (fedInc - 47150) * 0.22);
+            } else if (fedInc <= 191950) {
+                federalTax = (17168.50 + (fedInc - 100525) * 0.24);
+            } else if (fedInc <= 243725) {
+                federalTax = (39110.50 + (fedInc - 191950) * 0.32);
+            } else if (fedInc <= 609350) {
+                federalTax = (55678.50 + (fedInc - 243725) * 0.35);
+            } else {
+                federalTax = (183647.25 + (fedInc - 609350) * 0.37); //
+            }
+        }
+
+        if (filingstatus === 'mfj') {
+            if (fedInc <= 23200) {
+                federalTax = (fedInc * 0.10);
+            } else if (fedInc <= 94300) {
+                federalTax = (2320 + (fedInc - 23200) * 0.12);
+            } else if (fedInc <= 201050) {
+                federalTax = (10852 + (fedInc - 94300) * 0.22);
+            } else if (fedInc <= 383900) {
+                federalTax = (34337 + (fedInc - 201050) * 0.24);
+            } else if (fedInc <= 487450) {
+                federalTax = (78221 + (fedInc - 383900) * 0.32);
+            } else if (fedInc <= 731200) {
+                federalTax = (111357 + (fedInc - 487450) * 0.35);
+            } else {
+                federalTax = (196669.50 + (fedInc - 731200) * 0.37); //
+            }
+        }
+
+        // State tax rate (example, you need to update this based on the specific state)
+        let stateTaxRate;
+        switch(state) {
+            case 'ny':
+                stateTaxRate = 0.06;
+                break;
+            case 'ca':
+                stateTaxRate = 0.06;
+                break;
+            case 'notax':
+                stateTaxRate = 0;
+                break;
+                // Add cases for more states as needed
+                default:
+                stateTaxRate = 0; // Default state tax rate
+        }
+
+        //const federalTax = income * federalTaxRate;
+        const stateTax = income * stateTaxRate;
+
+        document.getElementById('result').innerHTML = `
+            <p></p>
+            <p>State Marginal Tax Rate: ${stateTaxRate * 100}%</p>
+            <p>Federal Tax: $${federalTax.toFixed(0)}</p>
+            <p>State Tax: $${stateTax.toFixed(2)}</p>
+            <p>Fed income: $${fedInc.toFixed(2)}</p>
+            `;
+        }
--- perfin/cash/~archive/20240204 1123/taxCalculator.js
+++ perfin/cash/~archive/20240204 1123/taxCalculator.js
@@ -19,7 +19,7 @@ function calculateTax() {
 
     // Validate input
     if (isNaN(income) || state === 'none') {
-        document.getElementById('result').innerHTML = 'Please enter valid data.';
+        document.getElementById('result').innerHTML = 'Please enter valid income and select a state.';
         return;
     }
 
@@ -116,43 +116,16 @@ function calculateTax() {
     const formattedNecessarySpend = formatCurrency(necessarySpend);
 
     // Update result on the webpage
-    const resultElement = document.getElementById('result');
-    resultElement.innerHTML = `
+    document.getElementById('result').innerHTML = `
+        <p></p>
         <p>Monthly 'necessary' spend: ${formattedNecessarySpend}</p>
         <p>Monthly income after taxes: ${formattedMonthlyIncome}</p>
         <p>Sustainable budget: ${isSustainable ? 'Yes' : 'No'}</p>
     `;
-
-    // If the budget is sustainable, add a bulleted message
-    if (isSustainable) {
-        const messageElement = document.createElement('ul');
-        messageElement.innerHTML = `
-            <li>Your budget is sustainable (without having factored non-necessities)!</li>
-            <li>Make sure you're building an <a href="https://www.bogleheads.org/wiki/Emergency_fund" target="_blank">emergency fund</a>.</li>
-            <li>Park your savings in a <a href="https://www.nerdwallet.com/best/banking/high-yield-online-savings-accounts" target="_blank">high-yield savings account</a> - this is free cash.</li>
-            <li>More on this to come, but also make sure you are (i) adequetely insured, (ii) paying down any high-interest debt, and (iii) saving for retirement.</li>
-        `;
-        resultElement.appendChild(messageElement);
-    } else {
-    const messageElement = document.createElement('ul');
-    messageElement.innerHTML = `
-        <li>Your budget is not sustainable. Consider the following:</li>
-        <li>Decreasing expenses: this could be finding cheaper accomodations or other creative ways to reduce your spend.</li>
-        <li>Increasing income through additional work or investments.</li>
-        <li>As soon as you're able, make sure you're building an <a href="https://www.bogleheads.org/wiki/Emergency_fund" target="_blank">emergency fund</a>.</li>
-        <li>More on this to come, but also make sure you are (i) adequetely insured, (ii) paying down any high-interest debt, and (iii) saving for retirement.</li>
-    `;
-    resultElement.appendChild(messageElement);
-}
-
 }
 
 // Function to parse currency inputs
 function parseCurrency(value) {
-    // If value is empty or not provided, return 0
-    if (!value) {
-        return 0;
-    }
     // Remove non-numeric characters from input value
     const numericValue = value.replace(/[^\d.]/g, '');
     // Convert string to number and return
